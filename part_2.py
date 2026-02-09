79d9b7b95017fd4e348dd2c0b28d81dcac85f06e
ap110-kent <ap110@kent.ac.uk>
2026-02-09 12:08:46 +0000

    Part 2 is done

--- part_2.py
+++ part_2.py
@@ -1,1 +1,23 @@
+#Task 2.1 : system status 
+rover_status = {
+    "Battery" : 100 , 
+    "Heater"  : "off",
+    "Camera"  : "Standby"
+}
 
+print(rover_status["Battery"])
+
+#Task 2.2: Status Update 
+
+rover_status["Battery"] = 85
+rover_status["Heater"] = "On"
+rover_status["Speed"] = 5
+
+print(rover_status)
+
+#Task 2.3: The scince log (Nesting)
+
+mission_log = [
+    {"Site": "Crater A", "Radiation": "Low", "Water": False},
+    {"Site": "Dune B", "Radiation": "High", "Water": True}
+]
